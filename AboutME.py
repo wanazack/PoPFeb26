a0c65c396060f27ad28c37e2b97daf29ba2924a9
wanazack <iwana.zakaria@gmail.com>
2026-03-08 09:49:37 +0800

    Add files via upload

---
+++ AboutME.py
@@ -0,0 +1,12 @@
+import streamlit as st
+#Title of the app
+st.title("Taylor's Student")
+
+#Input: Student Details
+st.header("About me")
+
+st.text("I am also a Biotechnology Student")
+st.text("I cannot write a bicycle")
+st.text("I like to cook and eat")
+
+st.success("Success! Welcome to streamlit!")
